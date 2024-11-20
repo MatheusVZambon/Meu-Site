cdcd32efb8e6e34e760247f802afaf2aba6f4065
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-20 17:36:21 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,11 +1,5 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
-from ACTlib01 import *
-url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQFwxxM13bxUC0dpyd0w0PxfZIrJ-hp4Px-R6rsTiG3c3n-89JApzA0jYJpU9vNfxeNCvtJ0Cg35KtO/pub?gid=556192647&single=true&output=csv"
-db = Ler_GooglePlanilha(url)
-db.fillna('', inplace=True)
-Escrever(db)
-
 
 form = st.form("formHD")
 nome = st.text_input("Digite seu nome:")
