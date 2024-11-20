bd48fbe18266942247f81528c9ab29b88ab88e96
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-20 18:17:46 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,7 +4,7 @@ import streamlit as st
 form = st.form("formHD")
 nome = st.text_input("Digite seu nome:")
 email = st.text_input("Digite seu e-mail:")
-prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*")
+prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
 
 problema = st.selectbox("Qual o problema apresentado?",("Sem Internet", "Alteração de Senha", "Outros"))
 botao = st.button("Enviar")
