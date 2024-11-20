619d91f30e2a8a8197dc8a79b416feb6ea82932b
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-20 18:45:53 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,6 +2,7 @@
 import streamlit as st
 
 form = st.form("formHD")
+ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu nome:")
 email = st.text_input("Digite seu e-mail:")
 prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
@@ -9,6 +10,6 @@ assunto = st.text_input("Assunto: ")
 mensagem = st.text_input("Messagem: ")
 
 msg.add_attachment(file_data, maintype='application', subtype='octet-stream', filename=file_name)
-
+arquivo = open('dados.txt', 'r')
 #problema = st.selectbox("Qual o problema apresentado?",("Sem Internet", "Alteração de Senha", "Outros"))
 botao = st.button("Enviar")
