2285f5f409efe7e9b2af4d37301d44eb97bb347f
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-20 18:16:36 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,9 +3,8 @@ import streamlit as st
 
 form = st.form("formHD")
 nome = st.text_input("Digite seu nome:")
+email = st.text_input("Digite seu e-mail:")
+prioridade = st.selectbox("Baixa", "Medio", "Alta", "*Critico*")
+
 problema = st.selectbox("Qual o problema apresentado?",("Sem Internet", "Alteração de Senha", "Outros"))
-  if(st.selectbox == Outros){
-    problema = st.text_input("Qual o problema apresentado?")
-  }
-#equipamento = st.selectbox("Qual o equipamento com defeito?",("Mouse", "Computador","Monitor","Teclado","Impressora"))
 botao = st.button("Enviar")
