cb5f3622eb8638628f380005268e8fb6e8920fda
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-20 17:34:34 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -6,20 +6,6 @@ db = Ler_GooglePlanilha(url)
 db.fillna('', inplace=True)
 Escrever(db)
 
-# Use st.title("") para adicionar um TÍTULO ao seu Web app
-st.title("MEU 1º WEB APP STREAMLIT")
-
-# Use st.header("") para adicionar um CABEÇALHO ao seu Web app
-st.header("JONAS ALVES DE CARVALHO")
-
-# Use st.subheader("") para adicionar um SUB CABEÇALHO ao seu Web app
-st.subheader("Sub Cabeçalho")
-
-# Use st.write("") para adicionar um texto ao seu Web app
-st.write("Como já deve ter percebido, o método st.write() é usado para escrita de texto e informações gerais!")
-
-values = st.slider("Select a range of values", 0.0, 100.0, (5.0, 15.0))
-st.write("Values:", values)
 
 form = st.form("formHD")
 nome = st.text_input("Digite seu nome:")
