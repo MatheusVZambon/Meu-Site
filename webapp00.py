4f7a84d62c0530345eb9729f9f2d38a520d8e3ce
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 20:27:21 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -8,12 +8,5 @@ senha = st.text_input("Senha: ")
 botao = st.button("Enviar")
 
 if usuario == 'admin' and senha == 'admin':
- ID = st.text_input("Digite seu ID:")
- nome = st.text_input("Digite seu nome:")
- email = st.text_input("Digite seu e-mail:")
- prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
- assunto = st.text_input("Assunto: ")
- mensagem = st.text_input("Messagem: ")
-
- botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
-
+  pg = st.Page("page1.py")
+  pg.run()
