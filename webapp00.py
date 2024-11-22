b6c486ccb3329d0f5f555f1287a1b82dc9a2266d
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 19:32:06 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,7 +2,12 @@
 import streamlit as st
 
 #pg = st.navigation([st.Page("page1.py")])
+#pg.run()
 
+st.sidebar.title('Login')
+Page_client = st.sidebar.selectbox('Login', ['conta'])
+
+Page_
 form = st.form("formHD")
 ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu nome:")
@@ -12,4 +17,3 @@ assunto = st.text_input("Assunto: ")
 mensagem = st.text_input("Messagem: ")
 
 botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
-#pg.run()
