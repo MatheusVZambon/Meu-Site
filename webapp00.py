c1641569e4d44b21a7538ef2a6352cdba97ae68e
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 20:05:14 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -5,3 +5,7 @@ usuario = st.text_input("Digite seu Usuario: ")
 senha = st.text_input("Senha: ")
 
 botao = st.button("Enviar")
+
+if usuario == admin
+  pg = st.navigation([st.Page("page1.py")])
+  pg.run()
