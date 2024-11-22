0391a166de31b22fc7437825f5b7d39478da3715
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 20:43:13 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,11 +1,12 @@
 import streamlit as st
 
+pg = st.Page("page1.py")
+pg.run()
+
 form = st.form("formHD")
 usuario = st.text_input("Digite seu Usuario: ")
 senha = st.text_input("Senha: ")
 
 botao = st.button("Logar")
 
-pg = st.Page("page1.py")
-pg.run()
 
