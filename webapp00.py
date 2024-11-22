f5b8453030cb769f76897f9bf72a783be167bb23
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 19:33:19 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -5,7 +5,7 @@ import streamlit as st
 #pg.run()
 
 st.sidebar.title('Login')
-Page_client = st.sidebar.selectbox('Login', ['conta'])
+Page_client = st.sidebar('Login', ['conta'])
 
 form = st.form("formHD")
 ID = st.text_input("Digite seu ID:")
