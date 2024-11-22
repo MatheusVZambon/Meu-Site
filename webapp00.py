d06566e4374f9fc56a7c04f6683046730ea3bbf7
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 20:44:42 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,12 +1,11 @@
 import streamlit as st
 
-pg = st.Page("page1.py")
-pg.run()
-
 form = st.form("formHD")
 usuario = st.text_input("Digite seu Usuario: ")
 senha = st.text_input("Senha: ")
 
 botao = st.button("Logar")
 
-
+if usuario == 'admin' and senha == 'admin'
+  pg = st.navigation([st.Page("page1.py")])
+  pg.run()
