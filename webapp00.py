07f8929e9939a2b6ec9dcb57b407a4dcc953ada4
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 20:04:51 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,9 +1,6 @@
 # MEU PRIMEIRO WEB APP
 import streamlit as st
 
-pg = st.navigation([st.Page("page1.py")])
-pg.run()
-
 usuario = st.text_input("Digite seu Usuario: ")
 senha = st.text_input("Senha: ")
 
