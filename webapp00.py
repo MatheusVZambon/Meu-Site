2e0f5b8b2fb60fd33d0c964c36e8a0d88941f20b
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 20:34:19 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -5,9 +5,9 @@ form = st.form("formHD")
 usuario = st.text_input("Digite seu Usuario: ")
 senha = st.text_input("Senha: ")
 
-botao = st.button("Enviar",
+botao = st.button("Logar")
 
 if usuario == 'admin' and senha == 'admin':
   pg = st.Page("page1.py")
   pg.run()
-)
+
