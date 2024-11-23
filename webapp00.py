05e72fe0fec74efadaa1e53a54c10bbd855a563a
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:04:49 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,6 +1,18 @@
 import streamlit as st
 
-left, middle, right = st.columns(3, vertical_alignment=vertical_alignment)
+col1, col2, col3 = st.columns(3)
+
+with col1:
+    st.header("A cat")
+    st.image("https://static.streamlit.io/examples/cat.jpg")
+
+with col2:
+    st.header("A dog")
+    st.image("https://static.streamlit.io/examples/dog.jpg")
+
+with col3:
+    st.header("An owl")
+    st.image("https://static.streamlit.io/examples/owl.jpg")
   
 ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu nome:")
@@ -11,7 +23,6 @@ mensagem = st.text_input("Messagem: ")
 
 botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
 
-middle.image("https://static.streamlit.io/examples/dog.jpg")
 
 # This code example demonstrates how to read colored QR code with color background in Python.
 #import aspose.barcode as barcode
