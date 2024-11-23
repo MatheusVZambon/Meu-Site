de2a3f97cf9a8c08ead907b2e82e9cbbe316f9cd
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:05:22 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,8 +3,15 @@ import streamlit as st
 col1, col2, col3 = st.columns(3)
 
 with col1:
-    st.header("A cat")
-    st.image("https://static.streamlit.io/examples/cat.jpg")
+ID = st.text_input("Digite seu ID:")
+nome = st.text_input("Digite seu nome:")
+email = st.text_input("Digite seu e-mail:")
+prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
+assunto = st.text_input("Assunto: ")
+mensagem = st.text_input("Messagem: ")
+
+botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
+
 
 with col2:
     st.header("A dog")
@@ -14,14 +21,6 @@ with col3:
     st.header("An owl")
     st.image("https://static.streamlit.io/examples/owl.jpg")
   
-ID = st.text_input("Digite seu ID:")
-nome = st.text_input("Digite seu nome:")
-email = st.text_input("Digite seu e-mail:")
-prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
-assunto = st.text_input("Assunto: ")
-mensagem = st.text_input("Messagem: ")
-
-botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
 
 
 # This code example demonstrates how to read colored QR code with color background in Python.
@@ -38,5 +37,5 @@ botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2
 
 # Show results
 #for x in recognized_results:
-    print("Code Text: " + x.code_text)
-    print("Type: " + x.code_type_name)
+    #print("Code Text: " + x.code_text)
+    #print("Type: " + x.code_type_name)
