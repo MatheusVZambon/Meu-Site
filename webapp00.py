8d3bde2060d5554e85540cc6b41ef0febb4e4e52
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:09:31 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,14 +1,12 @@
 import streamlit as st
-import numpy as np
+import aspose.barcode as barcode
 
-col1, col2 = st.columns([3, 1])
-data = np.random.randn(10, 1)
-
-col1.subheader("A wide column with a chart")
-col1.line_chart(data)
-
-col2.subheader("A narrow column with the data")
-col2.write(data)
+reader = barcode.barcoderecognition.BarCodeReader("C:\\Files\\Sample_qr.jpg")
+reader.quality_settings.allow_complex_background = True  
+recognized_results = reader.read_bar_codes()
+for x in recognized_results:
+   print("Code Text: " + x.code_text)
+   print("Type: " + x.code_type_name)
 
 
 
@@ -20,21 +18,3 @@ assunto = st.text_input("Assunto: ")
 mensagem = st.text_input("Messagem: ")
 
 botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
-
-
-# This code example demonstrates how to read colored QR code with color background in Python.
-#import aspose.barcode as barcode
-
-# Load QR code image
-#reader = barcode.barcoderecognition.BarCodeReader("C:\\Files\\Sample_qr.jpg")
-
-# Specify quality settings to complex background
-#reader.quality_settings.allow_complex_background = True    
-
-# Read QR codes
-#recognized_results = reader.read_bar_codes()
-
-# Show results
-#for x in recognized_results:
-    #print("Code Text: " + x.code_text)
-    #print("Type: " + x.code_type_name)
