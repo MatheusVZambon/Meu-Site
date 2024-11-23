c7c6d7e279846f4a21896f2f2429b490b4357111
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:12:51 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -12,8 +12,11 @@ mensagem = st.text_input("Messagem: ")
 botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
 
 reader = barcode.barcoderecognition.BarCodeReader("C:\\Files\\Sample_qr.jpg")
+
 reader.quality_settings.allow_complex_background = True  
+
 recognized_results = reader.read_bar_codes()
+
 for x in recognized_results:
    print("Code Text: " + x.code_text)
    print("Type: " + x.code_type_name)
