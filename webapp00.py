f405e011d63b2b43b3ee58dc453f5041da2d2695
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:06:44 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,5 +1,16 @@
 import streamlit as st
 
+col1, col2 = st.columns([3, 1])
+data = np.random.randn(10, 1)
+
+col1.subheader("A wide column with a chart")
+col1.line_chart(data)
+
+col2.subheader("A narrow column with the data")
+col2.write(data)
+
+
+
 col1, col2, col3 = st.columns(3)
 
 with col1:
