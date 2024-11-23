cf1c45b6fe3b9d2114605b48e14355da1dafac13
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:00:28 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,17 +1,12 @@
 import streamlit as st
 
-form = st.form("formHD")
-usuario = st.text_input("Digite seu Usuario: ")
-senha = st.text_input("Senha: ")
+ID = st.text_input("Digite seu ID:")
+nome = st.text_input("Digite seu nome:")
+email = st.text_input("Digite seu e-mail:")
+prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
+assunto = st.text_input("Assunto: ")
+mensagem = st.text_input("Messagem: ")
 
-botao = st.button("Logar")
+botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
 
-if usuario == 'admin' and senha == 'admin':
- #pg = st.navigation([st.Page("page1.py")])
- #pg.run()
 
- def page_2():
-     st.title("Page 1")
-
- pg = st.navigation([st.Page("page1.py")]) 
- pg.run()
