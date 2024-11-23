c20e0f82241c32f60cb3e5837eb31a5be5cfc1c5
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:11:31 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,14 +1,6 @@
 import streamlit as st
 import aspose.barcode as barcode
 
-reader = barcode.barcoderecognition.BarCodeReader("C:\\Files\\Sample_qr.jpg")
-reader.quality_settings.allow_complex_background = True  
-recognized_results = reader.read_bar_codes()
-for x in recognized_results:
-   print("Code Text: " + x.code_text)
-   print("Type: " + x.code_type_name)
-
-
 
 ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu nome:")
@@ -18,3 +10,10 @@ assunto = st.text_input("Assunto: ")
 mensagem = st.text_input("Messagem: ")
 
 botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
+
+reader = barcode.barcoderecognition.BarCodeReader("C:\\Files\\Sample_qr.jpg")
+reader.quality_settings.allow_complex_background = True  
+recognized_results = reader.read_bar_codes()
+for x in recognized_results:
+   print("Code Text: " + x.code_text)
+   print("Type: " + x.code_type_name)
