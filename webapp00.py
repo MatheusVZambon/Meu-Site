edde50313c505685f39a87af86d37237949f6f28
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-11-22 21:17:38 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -11,7 +11,7 @@ mensagem = st.text_input("Messagem: ")
 
 botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
 
-reader = barcode.barcoderecognition.BarCodeReader("https://www.google.com/url?sa=i&url=https%3A%2F%2Fpt.wikipedia.org%2Fwiki%2FC%25C3%25B3digo_QR&psig=AOvVaw1FglxS9486JO_AmY7momXV&ust=1732407251569000&source=images&cd=vfe&opi=89978449&ved=0CBQQjRxqFwoTCNio64iW8YkDFQAAAAAdAAAAABAE")
+reader = barcode.barcoderecognition.BarCodeReader("C:\\Files\\Sample_qr.jpg")
 
 reader.quality_settings.allow_complex_background = True  
 
