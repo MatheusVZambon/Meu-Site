f600e1b0210cae0b33f4fc0b912a088a4846c997
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:07:25 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,11 +1,16 @@
 import streamlit as st
 
-ID = st.text_input("Digite seu ID:")
-nome = st.text_input("Digite seu nome:")
-email = st.text_input("Digite seu e-mail:")
-prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
-assunto = st.text_input("Assunto: ")
-mensagem = st.text_input("Messagem: ")
+left, middle, right = st.columns(3, vertical_alignment="bottom")
 
-botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
+left.text_input("Write something")
+middle.button("Click me", use_container_width=True)
+right.checkbox("Check me")
 
+left.ID = st.text_input("Digite seu ID:")
+left.nome = st.text_input("Digite seu nome:")
+left.email = st.text_input("Digite seu e-mail:")
+left.prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
+left.assunto = st.text_input("Assunto: ")
+left.mensagem = st.text_input("Messagem: ")
+
+middle.botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
