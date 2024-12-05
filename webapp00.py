9ba225c09870ba891a741df2b11f15338b371ebe
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 23:31:49 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -23,11 +23,6 @@ custom_html = """
 # Display the custom HTML
 st.components.v1.html(custom_html)
 
-# Sidebar content
-st.sidebar.header("Sidebar Title")
-st.sidebar.subheader("Subheading")
-st.sidebar.text("Sidebar content goes here.")
-
 # Main content
 st.title("Main Content")
 st.write("Welcome to my Streamlit app!")
