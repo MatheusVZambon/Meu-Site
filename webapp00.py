9a96d01534aa6e238ca15cbc1f2abfb8551d6aba
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:54:57 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -5,8 +5,8 @@ def vote(item):
     ID = st.text_input("Digite seu ID:")
     nome = st.text_input("Digite seu Nome:")
     senha = st.text_input("Digite sua Senha:")
-        if nome == 'admin' and senha == 'admin':
-            st.page("page_1.py")
+    if nome == 'admin' and senha == 'admin':
+        st.page("page_1.py")
 
 if "vote" not in st.session_state:
     if st.button("Login"): 
