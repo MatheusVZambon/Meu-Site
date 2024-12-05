1ce5f2d6226480f99a218afa12d7ff292063999f
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:43:23 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,8 +2,14 @@ import streamlit as st
 
 @st.dialog("Cast your vote")
 def vote(item):
-    st.write(f"Why is {item} your favorite?")
-    reason = st.text_input("Because...")
+    ID = st.text_input("Digite seu ID:")
+    nome = st.text_input("Digite seu nome:")
+    email = st.text_input("Digite seu e-mail:")
+    prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
+    assunto = st.text_input("Assunto: ")
+    mensagem = st.text_input("Messagem: ")
+
+    botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
     if st.button("Submit"):
         st.session_state.vote = {"item": item, "reason": reason}
         st.rerun()
