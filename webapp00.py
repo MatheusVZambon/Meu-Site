15f43c70cabdca25b7dc88849c818cc635995317
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 23:00:54 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,13 +1,8 @@
 import streamlit as st
 
-@st.dialog("Abri um Chamado")
-def vote(item):
-    ID = st.text_input("Digite seu ID:")
-    nome = st.text_input("Digite seu Nome:")
-    senha = st.text_input("Digite sua Senha:")
-    if nome == 'admin' and senha == 'admin':
-        st.page_link("page1.py")
+ID = st.text_input("Digite seu ID:")
+nome = st.text_input("Digite seu Nome:")
+senha = st.text_input("Digite sua Senha:")
+if nome == 'admin' and senha == 'admin':
+    st.page_link("page1.py")
     
-if "vote" not in st.session_state:
-    if st.button("Login"): 
-        vote("Login")
