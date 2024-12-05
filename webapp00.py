4eacb174c0ab744c5c16126975111a89038c9e95
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 21:41:23 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,36 +1,4 @@
 import streamlit as st
-#import cv2
-#import numpy as np
-import aspose.barcode as barcode
-
-reader = barcode.barcoderecognition.BarCodeReader("C:\Users\Administrador\Downloads\Sample_QR")
-reader.quality_settings.allow_complex_background = True  
-recognized_results = reader.read_bar_codes()
-for x in recognized_results:
-  print("Code Text: " + x.code_text)
-  print("Type: " + x.code_type_name)
-
-image = st.camera_input("Show QR code")
-
-if image is not None:
-    bytes_data = image.getvalue()
-    cv2_img = cv2.imdecode(np.frombuffer(bytes_data, np.uint8), cv2.IMREAD_COLOR)
-
-    detector = cv2.QRCodeDetector()
-
-    data, bbox, straight_qrcode = detector.detectAndDecode(cv2_img)
-
-    st.write("Here!")
-    st.write(data)
-
-
-
-
-
-
-
-
-
 
 ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu nome:")
