b29e633f7b2a8372e29cbcfc08af9ef2fe413c51
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:09:33 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-left, middle = st.columns(3, vertical_alignment="bottom")
+left, middle, right = st.columns(3, vertical_alignment="bottom")
 
 left.text_input("Write something")
 middle.button("Click me", use_container_width=True)
