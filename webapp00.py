5ecaa3f53351d8eea8f63a014f9b62702d85bf90
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 23:07:04 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -26,6 +26,26 @@ def form_page():
 
     # Criando o link para o Google Forms com os dados
     base_url = "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?"
-    params = {
+        params = {
         "entry.69514635": ID,
         "entry.1100048191": nome,
+        "entry.8340763": email,
+        "entry.398253139": prioridade,
+        "entry.618138322": assunto,
+        "entry.1550799906": mensagem
+    }
+
+    # Codificando os parâmetros para a URL
+    url = base_url + urllib.parse.urlencode(params)
+
+    # Botão de envio para o Google Forms
+    if st.button("Enviar"):
+        st.write("Formulário enviado com sucesso!")
+        # Redirecionar para o Google Forms com os dados preenchidos
+        st.markdown(f'<a href="{url}" target="_blank">Clique aqui para enviar seus dados</a>', unsafe_allow_html=True)
+
+# Verifica se o usuário está logado e decide qual página exibir
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    login_page()  # Exibe a página de login se o usuário não estiver logado
+else:
+    form_page()  # Exibe o formulário se o usuário estiver logado
