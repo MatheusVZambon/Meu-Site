954f47735e2d7d4d885b6a1d813a908261ca9723
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 23:03:21 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,6 +3,7 @@ import streamlit as st
 ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu Nome:")
 senha = st.text_input("Digite sua Senha:")
+botao = st.button('Enviar')
 if nome == 'admin' and senha == 'admin':
     st.page_link("page1.py")
     
