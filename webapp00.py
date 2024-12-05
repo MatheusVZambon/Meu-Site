f645b0d5a41661320aaa737f1d6dd7ce6894bc33
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:09:06 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,10 +1,9 @@
 import streamlit as st
 
-left, middle, right = st.columns(3, vertical_alignment="bottom")
+left, middle = st.columns(3, vertical_alignment="bottom")
 
 left.text_input("Write something")
 middle.button("Click me", use_container_width=True)
-right.checkbox("Check me")
 
 left.ID = st.text_input("Digite seu ID:")
 left.nome = st.text_input("Digite seu nome:")
@@ -13,4 +12,4 @@ left.prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Criti
 left.assunto = st.text_input("Assunto: ")
 left.mensagem = st.text_input("Messagem: ")
 
-middle.botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
+middle(botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+""))
