38490f928c9cc85677aaf24c19626a90ddd38ef3
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 23:03:38 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -4,6 +4,6 @@ ID = st.text_input("Digite seu ID:")
 nome = st.text_input("Digite seu Nome:")
 senha = st.text_input("Digite sua Senha:")
 botao = st.button('Enviar')
-if nome == 'admin' and senha == 'admin':
-    st.page_link("page1.py")
+    if nome == 'admin' and senha == 'admin':
+        st.page_link("page1.py")
     
