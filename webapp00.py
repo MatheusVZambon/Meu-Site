a2560232193cb80672509381b87f2e441712dede
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:49:32 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,25 +1,14 @@
 import streamlit as st
 
-@st.dialog("Cast your vote")
+@st.dialog("Abri um Chamado")
 def vote(item):
     ID = st.text_input("Digite seu ID:")
-    nome = st.text_input("Digite seu nome:")
-    email = st.text_input("Digite seu e-mail:")
-    prioridade = st.selectbox("Prioridade: ",("Baixa", "Medio", "Alta", "*Critico*"))
-    assunto = st.text_input("Assunto: ")
-    mensagem = st.text_input("Messagem: ")
-
-    botao = st.link_button("Enviar", "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?&submit=Submit?usp=pp_url&entry.69514635="+ID+"&entry.1100048191="+nome+"&entry.8340763="+email+"&entry.398253139="+prioridade+"&entry.618138322="+assunto+"&entry.1550799906="+mensagem+"")
-   
-
-ID = st.text_input("Digite seu ID:")
-nome = st.text_input("Digite seu Nome:")
-senha = st.text_input("Digite sua Senha:")
+    nome = st.text_input("Digite seu Nome:")
+    senha = st.text_input("Digite sua Senha:")
 
 
 if "vote" not in st.session_state:
     if st.button("Login"): 
-        if nome == 'admin' and senha == 'admin':
-            vote("Login")
+        ote("Login")
 else:
     f"You voted for {st.session_state.vote['item']} because {st.session_state.vote['reason']}"
