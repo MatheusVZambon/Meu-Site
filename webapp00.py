f4f0e6d5c2050bc53a5561f31bdea7b944ccde1c
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:19:21 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -2,11 +2,11 @@ import streamlit as st
 
 import streamlit as st
 
-left, middle = st.columns(2, vertical_alignment="bottom")
+left, middle = st.columns(1, vertical_alignment="bottom")
 
 
 ID = left.text_input("Digite seu ID:")
 nome = left.text_input("Digite seu nome:")
 senha = left.text_input("Digite seu e-mail:")
 
-botao = middle.button("Enviar")
+botao = left.button("Enviar")
