12f3f7d608afda26caaa3961430b75603f854406
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 23:29:30 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,7 +1,24 @@
 import streamlit as st
 import urllib.parse
 
-st.image("https://meialua.net/wp-content/uploads/2024/06/tectoy.png", use_container_width=False)
+st.set_page_config(layout="wide")
+# Custom HTML/CSS for the banner
+custom_html = """
+<div class="banner">
+    <img src="https://img.freepik.com/premium-photo/wide-banner-with-many-random-square-hexagons-charcoal-dark-black-color_105589-1820.jpg" alt="Banner Image">
+</div>
+<style>
+    .banner {
+        width: 160%;
+        height: 200px;
+        overflow: hidden;
+    }
+    .banner img {
+        width: 100%;
+        object-fit: cover;
+    }
+</style>
+"""
 # Função de login
 def login_page():
     ID = st.text_input("Digite seu ID:")
