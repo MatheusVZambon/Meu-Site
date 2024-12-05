a15d1d06ecddce564e791b75f54a3d30b7d11b93
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:21:55 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -6,7 +6,12 @@ left, middle = st.columns(2, vertical_alignment="bottom")
 
 
 ID = left.text_input("Digite seu ID:")
-nome = left.text_input("Digite seu nome:")
-senha = left.text_input("Digite seu e-mail:")
+nome = left.text_input("Digite seu Nome:")
+senha = left.text_input("Digite seu Senha:")
 
 botao = left.button("Enviar")
+
+st.page1.py(
+    page_title="Hello",
+    page_icon="👋",
+)
