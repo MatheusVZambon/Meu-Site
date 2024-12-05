1b69661429c28a3df04086862c64d8352b26dcb8
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 22:10:38 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -3,7 +3,6 @@ import streamlit as st
 left, middle, right = st.columns(3, vertical_alignment="bottom")
 
 left.text_input("Write something")
-middle.button("Click me", use_container_width=True)
 
 left.ID = st.text_input("Digite seu ID:")
 left.nome = st.text_input("Digite seu nome:")
