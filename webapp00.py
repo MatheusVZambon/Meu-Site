75f7b6bbc31cd944d3f9df364194dbbedf63a482
Jonas Alves de Carvalho <165728620+J0n4S-C4rv4lh0@users.noreply.github.com>
2024-12-04 23:06:24 -0300

    Update webapp00.py

--- webapp00.py
+++ webapp00.py
@@ -1,10 +1,31 @@
 import streamlit as st
+import urllib.parse
 
-ID = st.text_input("Digite seu ID:")
-nome = st.text_input("Digite seu Nome:")
-senha = st.text_input("Digite sua Senha:")
-botao = st.button('Enviar')
-if nome == 'admin' and senha == 'admin':
-   pg = st.navigation([st.Page("page1.py")])
-   pg.run()
-    
+# Função de login
+def login_page():
+    ID = st.text_input("Digite seu ID:")
+    nome = st.text_input("Digite seu nome:")
+    senha = st.text_input("Digite sua senha:", type="password")
+
+    if st.button("Login"):
+        if nome == 'admin' and senha == 'admin':  # Simples verificação de login
+            st.session_state.logged_in = True
+            st.session_state.page = "form"  # Redireciona para o formulário após login
+            st.success("Login bem-sucedido!")
+        else:
+            st.error("Nome de usuário ou senha incorretos.")
+
+# Função para o formulário
+def form_page():
+    ID = st.text_input("Digite seu ID:")
+    nome = st.text_input("Digite seu nome:")
+    email = st.text_input("Digite seu e-mail:")
+    prioridade = st.selectbox("Prioridade: ", ("Baixa", "Média", "Alta", "Crítico"))
+    assunto = st.text_input("Assunto: ")
+    mensagem = st.text_input("Mensagem: ")
+
+    # Criando o link para o Google Forms com os dados
+    base_url = "https://docs.google.com/forms/d/e/1FAIpQLSe0_2DQ533BSUEuWf9haVK7w6oUkMei_kFphG-MKGwvA9_bzQ/formResponse?"
+    params = {
+        "entry.69514635": ID,
+        "entry.1100048191": nome,
